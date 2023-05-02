d7701731043bb27915be6cf7ef0755c1a5f3d7be
Nikola Grbovic <grbovicn.ra@gmail.com>
2023-05-02 16:10:30 +0200

    initial commit

---
+++ pc-parts-be/src/main/java/com/iz/parts/demo/PcPartsApplication.java
@@ -0,0 +1,14 @@
+package com.iz.parts.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcPartsApplication {
+
+	public static void main(String[] args) {
+		System.out.print("Test");
+		SpringApplication.run(PcPartsApplication.class, args);
+	}
+
+}
---
+++ pc-parts-fe/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" style={{ width: '100%', height: '100vh', display: 'grid', placeItems: 'center' }}>
+      <div style={{width: '600px', height: '100px', fontSize: '3rem'}}>
+        Nema odustajanja 💪💪💪
+      </div>
+    </div>
+  );
+}
+
+export default App;
