14b092998c40cfb61af711dc6b7fff7a6371a343
Nikola Grbovic <grbovicn.ra@gmail.com>
2023-05-03 11:48:59 +0200

    load ontology

--- pc-parts-be/src/main/java/com/iz/parts/demo/PcPartsApplication.java
+++ pc-parts-be/src/main/java/com/iz/parts/demo/PcPartsApplication.java
@@ -1,7 +1,13 @@
 package com.iz.parts.demo;
 
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
 
 @SpringBootApplication
 public class PcPartsApplication {
@@ -9,6 +15,20 @@ public class PcPartsApplication {
 	public static void main(String[] args) {
 		System.out.print("Test");
 		SpringApplication.run(PcPartsApplication.class, args);
+		LoadOntology(new File("data/ontology.owl"));
+		System.out.print("Done");
+	}
+
+	public static OWLOntology LoadOntology(File file) {
+		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+		OWLOntology ontology = null;
+		try {
+			ontology = manager.loadOntologyFromOntologyDocument(file);
+			System.out.print(ontology);
+		} catch (OWLOntologyCreationException e) {
+			throw new RuntimeException(e);
+		}
+		return ontology;
 	}
 
 }
