20e0b08836ad2601dd3e9cd0fd60f01c7888b836
Nikola Grbovic <grbovicn.ra@gmail.com>
2023-05-03 22:23:50 +0200

    reinit BE

---
+++ pc-parts-be/src/main/java/com/example/iz/parts/PcPartsBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.iz.parts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@ComponentScan(basePackages = "com.example.iz.parts.controller")
+public class PcPartsBeApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PcPartsBeApplication.class, args);
+	}
+
+}
